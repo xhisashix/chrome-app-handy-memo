07041c5ee49642c42f9c0405a53a12bc7d0077bd
Hisashi Iwamoto <hisasi622@gmail.com>
2023-07-11 19:28:59 +0900

    コピー機能追加

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,16 +1,38 @@
-// 要素が読み込まれるまで待つ
-window.addEventListener("DOMContentLoaded", () => {
-  const memo = document.getElementById("memo");
-  const saveBtn = document.getElementById("save_btn");
+const memo = document.getElementById("memo");
+const saveBtn = document.getElementById("save_btn");
 
-  // click save button
-  saveBtn.addEventListener("click", () => {
-    chrome.storage.local.set({ memo: memo.value }, () => {
-      console.log("save memo");
-    });
-  });
-
-  chrome.storage.local.get(["memo"], (result) => {
-    if(result.memo) memo.value = result.memo;
-  });
+chrome.storage.local.get(["memo"], (result) => {
+  if (result.memo) memo.value = result.memo;
 });
+
+function saveMemo() {
+  chrome.storage.local.set({ memo: memo.value }, () => {
+    console.log("save memo");
+  });
+}
+
+function clearMemo() {
+  memo.value = "";
+  chrome.storage.local.set({ memo: "" }, () => {
+    console.log("clear memo");
+  });
+}
+
+function copyToClipboard() {
+  const copyText = document.getElementById("memo");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  // Flash message
+  const flash = document.getElementById("flash");
+  // add text
+  flash.innerText = "Copied!";
+  flash.style.display = "block";
+  setTimeout(() => {
+    flash.style.display = "none";
+  }, 500);
+}
+
+document.getElementById("memo").addEventListener("blur", saveMemo);
+document.getElementById("reset_btn").addEventListener("click", clearMemo);
+document.getElementById("copy_btn").addEventListener("click", copyToClipboard);
