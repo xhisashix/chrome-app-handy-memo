55b55c339a6162bd0e8ccb88f2833f8e5c8cd996
Hisashi Iwamoto <hisasi622@gmail.com>
2024-03-31 12:05:04 +0900

    Add options for square, date, time, and URL

--- src/ts/optionsClass.ts
+++ src/ts/optionsClass.ts
@@ -6,6 +6,10 @@ class optionsClass {
   option_sharp_1: HTMLInputElement;
   option_sharp_2: HTMLInputElement;
   option_sharp_3: HTMLInputElement;
+  option_square: HTMLInputElement;
+  option_date: HTMLInputElement;
+  option_time: HTMLInputElement;
+  option_url: HTMLInputElement;
 
   constructor() {
     const storage = new storageClass();
@@ -16,6 +20,10 @@ class optionsClass {
     this.option_sharp_1 = this.form_option.sharp_1 as HTMLInputElement;
     this.option_sharp_2 = this.form_option.sharp_2 as HTMLInputElement;
     this.option_sharp_3 = this.form_option.sharp_3 as HTMLInputElement;
+    this.option_square = this.form_option.square as HTMLInputElement;
+    this.option_date = this.form_option.date as HTMLInputElement;
+    this.option_time = this.form_option.time as HTMLInputElement;
+    this.option_url = this.form_option.url as HTMLInputElement;
   }
 
   /**
@@ -25,6 +33,10 @@ class optionsClass {
     this.option_sharp_1.value = await this.storage.getStorageItem("sharp_1");
     this.option_sharp_2.value = await this.storage.getStorageItem("sharp_2");
     this.option_sharp_3.value = await this.storage.getStorageItem("sharp_3");
+    this.option_square.value = await this.storage.getStorageItem("square");
+    this.option_date.value = await this.storage.getStorageItem("date");
+    this.option_time.value = await this.storage.getStorageItem("time");
+    this.option_url.value = await this.storage.getStorageItem("url");
   }
 }
 
